9b90102793022f33aa77fee491ddf0a96c781ee2
daeho <679748@naver.com>
2025-03-15 20:37:13 +0900

    kotlin_valuable

---
+++ src/practice/Variable.kt
@@ -0,0 +1,35 @@
+package practice
+
+fun main(){;
+    var intNum: Int = 2;
+    var numNull :Int? = null; // null able 변수
+    var hexNum : Int = 0x1af;
+    var binNum : Int = 0b101010;
+    var longNum: Long = 2L;
+
+    var doubleNum : Double = 123.4;
+    var floatNum : Float = 0.1f;
+    var charValue : Char = 'a';
+    var charKorValue : Char = '가';
+    var booleanValue : Boolean = true;
+    var stringValue : String = "abcdef";
+    var multiLineStringValue : String = """HI 
+        HELLO STRING
+    """
+
+    println(intNum);
+    println(numNull);
+    println(longNum);
+    println(binNum);
+    println(hexNum);
+    println(doubleNum);
+    println(floatNum);
+    println(charValue);
+    println(charKorValue);
+    println(booleanValue);
+    println(stringValue);
+    println(multiLineStringValue);
+
+
+
+}
